4746fb963d32c4c8ce0faf74227293c9db18a6a9
Yan Bolondz <chessmachine18@gmail.com>
2025-10-07 19:23:46 +0300

    Добавлен проект CS_LAB03 в LAB03

---
+++ LAB03/CS_LAB03/CS_LAB03/CS_LAB03.cpp
@@ -0,0 +1,12 @@
+﻿/ using System;
+
+namespace CS_LAB03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №3 по C#");
+        }
+    }
+
