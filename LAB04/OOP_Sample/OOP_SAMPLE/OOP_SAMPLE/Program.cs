4a33dd18b33a0f00696e1853318998c5b4537735
Yan Bolondz <chessmachine18@gmail.com>
2025-12-02 17:00:33 +0300

    4 ЛАБА

---
+++ LAB04/OOP_Sample/OOP_SAMPLE/OOP_SAMPLE/Program.cs
@@ -0,0 +1,32 @@
+﻿// Создание уравнения без корней
+using OOP_SAMPLE;
+
+QuadraticEquation eq1 = new QuadraticEquation(2, -4, 3);
+eq1.PrintInfo();
+
+QuadraticEquation eq2 = new QuadraticEquation(0, 4, -5);
+eq2.PrintInfo();
+
+
+QuadraticEquation eq3 = new QuadraticEquation(1, 2, 3);
+eq3.PrintInfo();
+Console.WriteLine(eq3.GetRootsCount());
+
+
+Console.WriteLine("Get Roots Of Equation 1:");
+foreach (double root in eq1.GetRoots())
+{
+    Console.WriteLine($"Root = {root}");
+}
+
+Console.WriteLine("Get Roots Of Equation 2:");
+foreach (double root in eq2.GetRoots())
+{
+    Console.WriteLine($"Root = {root}");
+}
+
+Console.WriteLine("Get Roots Of Equation 3:");
+foreach (double root in eq3.GetRoots())
+{
+    Console.WriteLine($"Root = {root}");
+}
